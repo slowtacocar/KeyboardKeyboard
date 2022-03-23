3c6af7b038e5685f55cd9d723586c0e6a5ea488e
Bobby George <slowtacocar@gmail.com>
2022-03-23 10:05:20 -0400

    Fix it

--- public/index.js
+++ public/index.js
@@ -1,12 +1,14 @@
 let root = 0;
 const intervals = [2, 2, 1, 2, 2, 2, 1];
 const roots = [0, 2, 4, 5, 7, 9, 11];
+const audios = [];
 let note = -1;
 const toPause = [];
 let beat = 1;
 
 for (let i = 0; i < 25; i++) {
-  const audio = new Audio(`audio/${i}.wav`);
+  const audio = new Audio(`audio/${i}.mp3`);
+  audios.push(audio);
   audio.load();
 }
 
@@ -16,14 +18,13 @@ window.setInterval(() => {
       root = Math.floor(Math.random() * 7);
       note = roots[root];
     }
-    const audio = new Audio(`audio/${note}.wav`);
-    audio.play();
+    audios[note].play();
     if (beat === 1) {
       while (toPause.length > 1) {
         toPause.pop().pause();
       }
     }
-    toPause.push(audio);
+    toPause.push(audios[note]);
     note = -1;
   }
   beat = beat >= 16 ? 1 : beat + 1;
